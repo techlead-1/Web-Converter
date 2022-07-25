c592cb0c8c9bf96f0b8fc1a170aa01dffe73ee18
techlead-1 <adesanya1ademola@gmail.com>
2022-07-25 07:09:39 +0100

    Api intergration for main html

---
+++ myApp.js
@@ -0,0 +1,11 @@
+//Setting up angular module
+var app = angular.module("myApp", []);
+
+//Angular controller for Angular app "myApp"
+app.controller('myCtrl', function($scope) {
+    $scope.clicked = false;
+
+    $scope.download = function() {
+        alert('Download will start soon...')
+    }
+})
