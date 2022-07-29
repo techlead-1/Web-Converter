77967e2d764fb7bb40557941ca47341f82f59e1a
techlead-1 <adesanya1ademola@gmail.com>
2022-07-29 04:11:58 +0100

    Advanced validation added

--- myThirdApp.js
+++ myThirdApp.js
@@ -2,27 +2,66 @@
 var app = angular.module("myThirdApp", []);
 
 //Angular controller for Angular app "myApp"
-app.controller('myThirdCtrl', function($scope) {
+app.controller('myThirdCtrl', function($scope, $interval, $http) {
     $scope.webUrl = ""
+    $scope.status = "";
+    $scope.check = true;
 
-    $scope.download = function(webUrl) {
-        //variable that searches for the http:// in string
-        $scope.search = $scope.webUrl.search("http://");
-        //variable to ccheck if string exist and is written at the beginning
-        $scope.httpResult = $scope.search >= 0;
-        
-        //variable that searches for https:// in string
-        $scope.searchTwo = $scope.webUrl.search("https://");
-        //variable to check if string exist and is written at the beginning
-        $scope.httpsResult = $scope.searchTwo >= 0;
-
-        //creating if condition to add an http:// prefix if both parameters do not exist
-        if($scope.httpResult || $scope.httpsResult) {
-            alert("Valid Url, download will start soon..")
-        }
-        else{
+    //Creating function that will be used to validate url input
+    var validateInput = function() {
+        //script will first check if an http or https input is added and will add if not found dynamically
+        $scope.httpSearch = $scope.webUrl.search("http");
+        $scope.httpCompare = $scope.httpSearch < 0;
+        if($scope.httpCompare && $scope.webUrl.length >= 4) {
             $scope.webUrl = "http://" + $scope.webUrl;
-            alert("Url has been auto-corrected and download will start soon..")
-        };
+            $scope.validationCount = 1;
+        }
+        else{$scope.webUrl = $scope.webUrl;
+             $scope.validationCount = 1;
+        }
+
+        //script will check if a domain extension is added and will automatically add a .com after a warning
+        //array of the most common domain
+        $scope.extensionSearch = $scope.webUrl.lastIndexOf(".");
+        $scope.extensionCompare = $scope.extensionSearch + 1< $scope.webUrl.length && $scope.extensionSearch + 1;
+        if($scope.extensionCompare && $scope.webUrl.length > 8 && $scope.extensionSearch - $scope.webUrl.length <= 4) {
+            $scope.status = "";
+            $scope.validationCountTwo = 1;
+        }
+        else if($scope.extensionSearch + 1 == $scope.webUrl.length) {
+            $scope.status = "Please add a .com or an equivalent domain extension";
+            $scope.validationCountTwo = 0;
+        }
+        else if($scope.extensionSearch == -1) {
+            $scope.status = "Please add a .com or an equivalent domain extension";
+            $scope.validationCountTwo = 0;
+        }
+
+        //last validation before unlocking button is space check
+        $scope.spaceSearch = $scope.webUrl.search(" ");
+        $scope.spaceLocation = $scope.spaceSearch + 1;
+        if ($scope.spaceSearch < 0) {
+            $scope.status = "";
+            $scope.validationCountThree = 1;
+        }
+        else if ($scope.spaceSearch >= 0) {
+            $scope.status = "Remove the space detected in row " + $scope.spaceLocation;
+            $scope.validationCountThree = 0
+        }
+
+        //Combining validation to unlock the download button
+        $scope.validationKeys = $scope.validationCount + $scope.validationCountTwo + $scope.validationCountThree;
+        //creating a condition that counts key, if keys are up to three, the download button is unlocked
+        if ($scope.validationKeys == 3) {
+            return $scope.check = false;
+        }
+        else{return $scope.check = true}
+    }
+
+    $interval(validateInput, 100)//carries out function every 100millisecs
+
+    //Creating a function to alert when api is processing data
+    $scope.getResponse = function() {
+        alert("Success, you download will start soon");
     }
 })
